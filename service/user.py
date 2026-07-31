bd2cbaef6ab8a6defaf28dcdc0d3da9250a98bf1
YuyangSong <songyuyang@beepluxsemi.com>
2026-07-31 15:57:34 +0800

    feat(service): implement user existence check

---
+++ service/user.py
@@ -0,0 +1,31 @@
+# @Author: LeonSong
+# @Date:   2026-07-31 14:21
+# @Description: Operations of user
+
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from schemas.user import UserCreate
+from models.users import Users
+
+
+def is_user_existed(session: Session, phone_number: str):
+    stmt = select(Users).where(Users.phone_number == phone_number)
+    res = session.execute(stmt).scalar_one_or_none()
+
+    return res is not None
+
+
+def create_user(session: Session, user_create: UserCreate):
+    # check user is existed
+
+    # generate number
+    pass
+
+
+if __name__ == "__main__":
+    from db.session import SessionLocal
+
+    with SessionLocal() as session:
+        res = is_user_existed(session, "122342")
+        print(res)
