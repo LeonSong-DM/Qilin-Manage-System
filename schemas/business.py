e26cf1ceb9503c8633cff31c3206a7c275cdafed
LeonSong <le0n404@163.com>
2026-08-01 12:20:17 +0800

    feat(schemas): add some business schemas

---
+++ schemas/business.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# @Author: LeonSong
+# @Date:   2026-08-01 11:44
+# @Description:  Schemas of business
+
+from pydantic import BaseModel, Field
+
+
+class UnitCreate(BaseModel):
+    name: str = Field(min_length=1, max_length=8)
+
+
+class OutboundRecordCreate(BaseModel):
+    outboud_number: str = Field(min_length=12, max_length=12)
+    outbound_quantity: int = Field(gt=0)
+    outboud_weight: int = Field(gt=0)
+
+
+class ProcessMethodCreate(BaseModel):
+    method_name: str = Field(min_length=1, max_length=16)
+
+
+class ProcessOptionCreate(BaseModel):
+    option_name: str = Field(min_length=1, max_length=16)
+    process_method_id: int = Field()
+
+
+class ClientCreateCreate(BaseModel):
+    client_number: str = Field(min_length=14, max_length=14)
+    client_name: str = Field(min_length=1, max_length=64)
+    contact_phone_number: str = Field(
+        min_length=11, max_length=11, pattern=r"^1[3-9]\d{9}$"
+    )
+    address: str = Field(max_length=255)
